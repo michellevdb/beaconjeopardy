fd1b8eef180560afd0c688225ce8ce9e37a1d509
Michelle <michellecyou@gmail.com>
2020-02-12 14:42:32 -0800

    added animations and changed topics

---
+++ script.js
@@ -0,0 +1,263 @@
+answershow = "choose a question"
+add = 0
+minus = 0 
+
+function data100(){
+	var data100win = window.open("", "data100win", "height=300, width=500, top=200, left=200");
+	data100win.document.write("<title>Phases 100</title><font size=6>Number of versions of Beacon and their names");
+	data100win.document.close();
+	answershow = "<font size=6><title>Phases 100 Answer</title>Three; Starter, Core, 24/7";
+	add = 100
+	minus = 100
+}
+
+function data200(){
+	var data200win = window.open("", "data100win", "height=300, width=500, top=200, left=200");
+	data200win.document.write("<title>Phases 200</title><font size=6>The Five Phases in Beacon");
+	data200win.document.close();
+	answershow = "<font size=6><title>Phases 200 Answer</title>Active Planning; Suicide Ideation; Self-Harm; Suicide Research";
+	add = 200
+	minus = 200
+}
+
+function data300(){
+	var data300win = window.open("", "data100win", "height=300, width=500, top=200, left=200");
+	data300win.document.write("<title>Phases 300</title><font size=6>The difference between Beacon Core and 24/7");
+	data300win.document.close();
+	answershow = "<font size=6><title>Phases 300 Answer</title>24/7 provides human review for all active planning alerts and will telephonically notify the school's escalation list when an alert is a verified Active Planning alert";
+	add = 300
+	minus = 300
+}
+
+function data400(){
+	var data400win = window.open("", "data100win", "height=300, width=500, top=200, left=200");
+	data400win.document.write("<title>Phases 400</title><font size=6>The act of changing a representation to user fewer bits for storage.</font>");
+	data400win.document.close();
+	answershow = "What is Compression";
+	add = 400
+	minus = 400
+}
+
+function data500(){
+	var data500win = window.open("", "data100win", "height=300, width=500, top=200, left=200");
+	data500win.document.write("<title>Phases 500</title><font size=6>The act of taking measurements at regular intervals when digitizing sounds.</font>");
+	data500win.document.close();
+	answershow = "What is Sampling";
+	add = 500
+	minus = 500
+}
+
+function history100(){
+	var history100win = window.open("", "data100win", "height=300, width=500, top=200, left=200");
+	history100win.document.write("<title>history100</title><font size=6>The inventor of the Difference Machine, the first automated computing system.</font>");
+	history100win.document.close();
+	answershow = "Who was Charles Babbage";
+	add = 100
+	minus = 100
+}
+
+function history200(){
+	var history200win = window.open("", "data100win", "height=300, width=500, top=200, left=200");
+	history200win.document.write("<title>history200</title><font size=6>The inventor of the first computer mouse.</font>");
+	history200win.document.close();
+	answershow = "Who was Douglas Engelbart";
+	add = 200
+	minus = 200
+}
+
+function history300(){
+	var history300win = window.open("", "data100win", "height=300, width=500, top=200, left=200");
+	history300win.document.write("<title>history300</title><font size=6>The amount of generations the computer has gone through.</font>");
+	history300win.document.close();
+	answershow = "What is 5";
+	add = 300
+	minus = 300 
+}
+
+function history400(){
+	var history400win = window.open("", "data100win", "height=300, width=500, top=200, left=200");
+	history400win.document.write("<title>history400</title><font size=6>The year of the first keyboard patent.</font>");
+	history400win.document.close();
+	answershow = "When was 1714";
+	add = 400
+	minus = 400
+}
+
+function history500(){
+	var history500win = window.open("", "data100win", "height=300, width=500, top=200, left=200");
+	history500win.document.write("<title>history500</title><font size=6>Considered to be the first high level programming language.</font>");
+	history500win.document.close();
+	answershow = "What is Short Code";
+	add = 500
+	minus = 500
+}
+
+function software100(){
+	var software100win = window.open("", "software100win", "height=300, width=500, top=200, left=200");
+	software100win.document.write("<title>software100</title><font size=6>Thing that stores running programs and data on computers.</font>");
+	software100win.document.close();
+	answershow = "What is memory";
+	add = 100
+	minus = 100
+}
+
+function software200(){
+	var software200win = window.open("", "software200win", "height=300, width=500, top=200, left=200");
+	software200win.document.write("<title>software200</title><font size=6>Performs math on a computer.</font>");
+	software200win.document.close();
+	answershow = "What is the Arithmetic Logic Unit";
+	add = 200
+	minus = 200
+}
+
+function software300(){
+	var software300win = window.open("", "software300win", "height=300, width=500, top=200, left=200");
+	software300win.document.write("<title>software300</title><font size=6>The language that computers read to carry out instructions.</font>");
+	software300win.document.close();
+	answershow = "What is binary";
+	add = 300
+	minus = 300
+}
+
+function software400(){
+	var software400win = window.open("", "software400win", "height=300, width=500, top=200, left=200");
+	software400win.document.write("<title>software400</title><font size=6>The five step process that summarizes how computers run.</font>");
+	software400win.document.close();
+	answershow = "What is the Fetch Execute Cycle";
+	add = 400
+	minus = 400
+}
+
+function software500(){
+	var software500win = window.open("", "software500win", "height=300, width=500, top=200, left=200");
+	software500win.document.write("<title>software500</title><font size=6>Procedures that allow programs to make contact with the Operating System.</font>");
+	software500win.document.close();
+	answershow = "What are Application Programming Interfaces (API)";
+	add = 500
+	minus = 500
+}
+
+function hardware100(){
+	var hardware100win = window.open("", "hardware100win", "height=300, width=500, top=200, left=200");
+	hardware100win.document.write("<title>hardware100</title><font size=6> Part of the computer that holds the Memory and CPU. </font>");
+	hardware100win.document.close();
+	answershow = "What is the motherboard";
+	add = 100
+	minus = 100
+}
+
+function hardware200(){
+	var hardware200win = window.open("", "hardware200win", "height=300, width=500, top=200, left=200");
+	hardware200win.document.write("<title>hardware200</title><font size=6>The amount of memory that a computer can store at one time.</font>");
+	hardware200win.document.close();
+	answershow = "What is storage capacity";
+	add = 200
+	minus = 200
+}
+
+function hardware300(){
+	var hardware300win = window.open("", "hardware300win", "height=300, width=500, top=200, left=200");
+	hardware300win.document.write("<title>hardware300</title><font size=6> Main high speed memory that holds data and instructions.</font>");
+	hardware300win.document.close();
+	answershow = "What is Random Access Memory (RAM)";
+	add = 300
+	minus = 300
+}
+
+function hardware400(){
+	var hardware400win = window.open("", "hardware400win", "height=300, width=500, top=200, left=200");
+	hardware400win.document.write("<title>hardware400</title><font size=6>Controls all other parts of the computer.</font>");
+	hardware400win.document.close();
+	answershow = "What is the control unit";
+	add = 400
+	minus = 400
+}
+
+function hardware500(){
+	var hardware500win = window.open("", "hardware500win", "height=300, width=500, top=200, left=200");
+	hardware500win.document.write("<title>hardware500</title><font size=6>Circuits that carry data from one area to another.</font>");
+	hardware500win.document.close();
+	answershow = "What are busses";
+	add = 500
+	minus = 500
+}
+
+
+//shows answer to question
+
+
+function answer(){
+	var answerwin = window.open("", "answerwin", "height=300, width=500, top=200, left=200")
+	answerwin.document.write(answershow)
+	answerwin.document.close()
+}
+
+
+
+
+//add value of selected button to selected group score 
+
+function correct(){
+	if (document.getElementById('groupone').checked) {
+		document.getElementById('p1').value = parseInt(document.getElementById('p1').value) + parseInt(add)
+		add = 0
+	}
+	if (document.getElementById('grouptwo').checked) {
+		document.getElementById('p2').value = parseInt(document.getElementById('p2').value) + parseInt(add)
+		add = 0
+	}
+	if (document.getElementById('groupthree').checked) {
+		document.getElementById('p3').value = parseInt(document.getElementById('p3').value) + parseInt(add)
+		add = 0
+	}
+	if (document.getElementById('groupfour').checked) {
+		document.getElementById('p4').value = parseInt(document.getElementById('p4').value) + parseInt(add)
+		add = 0
+	}
+	if (document.getElementById('groupfive').checked) {
+		document.getElementById('p5').value = parseInt(document.getElementById('p5').value) + parseInt(add)
+		add = 0
+	}
+	if (document.getElementById('groupsix').checked) {
+		document.getElementById('p6').value = parseInt(document.getElementById('p6').value) + parseInt(add)
+		add = 0
+	}
+}
+
+
+
+
+//subtracts value of selected button to selected group score 
+
+function wrong(){
+
+	if (document.getElementById('groupone').checked) {
+		document.getElementById('p1').value = parseInt(document.getElementById('p1').value) - parseInt(minus)
+		minus = 0
+	}
+
+	if (document.getElementById('grouptwo').checked) {
+		document.getElementById('p2').value = parseInt(document.getElementById('p2').value) - parseInt(minus)
+		minus = 0 
+	}
+	
+	if (document.getElementById('groupthree').checked) {
+		document.getElementById('p3').value = parseInt(document.getElementById('p3').value) - parseInt(minus)
+		minus = 0
+	}
+
+	if (document.getElementById('groupfour').checked) {
+		document.getElementById('p4').value = parseInt(document.getElementById('p4').value) - parseInt(minus)
+		minus = 0
+	}
+
+	if (document.getElementById('groupfour').checked) {
+		document.getElementById('p5').value = parseInt(document.getElementById('p5').value) - parseInt(minus)
+		minus = 0
+	}
+
+	if (document.getElementById('groupsix').checked) {
+		document.getElementById('p6').value = parseInt(document.getElementById('p6').value) - parseInt(minus)
+		minus = 0
+	}
+}
